c6ea43b3a005398c1c2d3e16a47f238c85ee473b
Tất Quảng Kiệt <52742909+tatquangkiet789@users.noreply.github.com>
2022-03-13 12:53:17 +0700

    Add SpringMVC Template

---
+++ src/main/java/com/tqk/configs/WebApplicationContextConfigs.java
@@ -0,0 +1,37 @@
+package com.tqk.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+/**
+ *
+ * @author Admin
+ */
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages = {
+        "com.tqk.controllers"
+})
+public class WebApplicationContextConfigs implements WebMvcConfigurer {
+
+    @Override
+    public void configureDefaultServletHandling(DefaultServletHandlerConfigurer configurer) {
+       configurer.enable();
+    }
+    
+    @Bean
+    public InternalResourceViewResolver getInternalResourceViewResolver() {
+        InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+        resolver.setViewClass(JstlView.class);
+        resolver.setPrefix("/WEB-INF/pages/");
+        resolver.setSuffix(".jsp");
+        
+        return resolver;
+    }
+}
---
+++ src/main/webapp/WEB-INF/pages/index.jsp
@@ -0,0 +1,17 @@
+<%-- 
+    Document   : index
+    Created on : Mar 13, 2022, 7:29:22 AM
+    Author     : Admin
+--%>
+
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+        <title>Homepage</title>
+    </head>
+    <body>
+        <h1>Hello World!</h1>
+    </body>
+</html>
